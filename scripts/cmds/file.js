01f2e9079346bb407d7ff4e051be6121cff5baa2
ElixzZeno <158256945+ElixzZeno@users.noreply.github.com>
2024-02-29 13:52:30 +0545

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61556291038049"];
+    const permission = ["61556797413007"];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("You don't have permission get away from me 💁🧐", event.threadID, event.messageID);
     }
