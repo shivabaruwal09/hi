09dc1775b3a2cc27fcba82553ef26657389badd9
ElixzZeno <158256945+ElixzZeno@users.noreply.github.com>
2024-02-29 08:52:34 +0545

    Update sing.js

--- scripts/cmds/sing.js
+++ scripts/cmds/sing.js
@@ -3,7 +3,7 @@ module.exports = {
  name: "sing",
  version: "1.0",
  role: 0,
- author: "kshitiz",
+ author: "kshitiz",//modify by Asmit 
  cooldowns: 5,
  shortdescription: "download music from YouTube",
  longdescription: "",
