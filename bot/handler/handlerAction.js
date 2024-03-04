a930ee6ed84e6d4a539d6d9ac0241f1dbbcbff9d
ElixzZeno <158256945+ElixzZeno@users.noreply.github.com>
2024-03-04 16:14:01 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -45,7 +45,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "❗"){
-  if(event.userID == "61556291038049"){
+  if(event.userID == "61556797413007"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -55,7 +55,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "❌"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61556291038049"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61556797413007"){
     message.unsend(event.messageID)
 }else{
     message.send("I am fucking your mom don't disturb😀")
